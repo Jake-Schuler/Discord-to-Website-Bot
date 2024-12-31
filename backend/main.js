6845ed8fe54b41e950a3a51d66936833bd95d001
Jake Schuler <jschuler@hackclub.app>
2024-12-31 15:43:52 -0600

    fix some stuff

--- backend/main.js
+++ backend/main.js
@@ -59,5 +59,5 @@ app.get('/', async (_req, res) => {
 client.login(Deno.env.get("DISCORD_TOKEN"));
 
 app.listen(Deno.env.get("BACKEND_PORT"), () => {
-    console.log(`Example app listening on port ${Deno.env.get("BACKEND_PORT")}`)
+    console.log(`Backend running on port ${Deno.env.get("BACKEND_PORT")}`)
 });
